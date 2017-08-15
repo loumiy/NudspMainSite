8d5097623e581a02d84674caa690778c8f131246
DSPWebmaster <nudsp.webmaster@gmail.com>
2017-08-15 18:59:21 -0400

    Header Now Selects Active Link
    Theoretically, when a page is loaded, the link corresponding to that page should have a "active" class added and the previous page's link should have the "active" class removed. Will need to be tested on master to confirm. Further tweaks on master expected.

--- utilityfunctions.js
+++ utilityfunctions.js
@@ -1,9 +1,12 @@
 // This function will create a NavBar on which ever HTML document it is called from
-function showNavBar() {
+function showNavBar(currentPage) {
     var link = document.querySelector('link[rel="import"]');
     var content = link.import;
+
+    $(".active").removeClass("active");
 
     var el = content.querySelector('.navbar');
 
     document.body.appendChild(el.cloneNode(true));
+    $("#" + currentPage).addClass("active");
 }
