30e01337fb75108ef98acc1d92b0efe1aeb9b3cb
DSPWebmaster <nudsp.webmaster@gmail.com>
2017-08-15 19:26:37 -0400

    Active Links Are Now Disabled
    Users can no longer select NavBar links that are currently active.

--- utilityfunctions.js
+++ utilityfunctions.js
@@ -3,10 +3,15 @@ function showNavBar(currentPage) {
     var link = document.querySelector('link[rel="import"]');
     var content = link.import;
 
-    $(".active").removeClass("active");
+    // Locate the element with the class "active". Remove the active and disabled classes so the link will once again
+    // work.
+    // NOTE: This works only because only one link can be active at a time
+    $(".active").removeClass("active disabled");
 
     var el = content.querySelector('.navbar');
 
     document.body.appendChild(el.cloneNode(true));
-    $("#" + currentPage).addClass("active");
+
+    // Locate the element for the current page and add the active and disabled classes
+    $("#" + currentPage).addClass("active disabled");
 }
